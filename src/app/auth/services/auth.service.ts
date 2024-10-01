8db30164a5aeaef161e488802819391ae771c2f1
M-Naill <mahmoud.naill7@gmail.com>
2024-10-02 02:50:30 +0300

    login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginRequest } from '../interfaces/login-request';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  role: string | null = '';
   constructor(private httpClient: HttpClient) {}
   url = 'auth/';
+
+  onLogin(data: LoginRequest): Observable<LoginRequest> {
+    return this.httpClient.post<LoginRequest>('Login', data);
+  }
+
+  getUserData() {
+    //get the token from the local storage
+    let encodeToken: any = localStorage.getItem('userToken');
+    let decoded: any = jwtDecode(encodeToken); //token is decoded
+    console.log(decoded);
+    localStorage.setItem('userEmail', decoded.userEmail);
+    localStorage.setItem('userName', decoded.userName);
+    localStorage.setItem('role', decoded.userGroup);
+    this.getRole();
+  }
+
+  getRole() {
+    if (
+      localStorage.getItem('userToken') !== null &&
+      localStorage.getItem('role') !== null
+    ) {
+      this.role = localStorage.getItem('role');
+    }
+  }
+
 }
