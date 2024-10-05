e8805b7bf5db754834d884a2488d5ef2b95b5308
Ola Fawzi <olafawzii80@gmail.com>
2024-10-05 18:01:46 +0300

    update

--- src/app/feature/dashboard/instructor/modules/quiz/quiz.component.ts
+++ src/app/feature/dashboard/instructor/modules/quiz/quiz.component.ts
@@ -1,43 +1,39 @@
 import { Component } from '@angular/core';
-import { DashlistService } from '../../dashlist.service';
+import { DashlistService } from '../../../services/dashlist.service';
+import { Quiz } from '../../../interfaces/quiz';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
-export class QuizComponent  {
-  constructor(private _DashlistService:DashlistService){}
-  students:any
-  images: string[] = [
-    '../../../../../assets/images/user img.svg',
-    '../../../../../assets/images/user img.png',
-    '../../../../../assets/images/2.svg',
-    '../../../../../assets/images/4.png',
-    '../../../../../assets/images/user img.svg',
-  ];
-  data: any[] = [];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = this.data;
+export class QuizComponent {
+  completedQuizzes: Quiz[] = [];
+  IncomingQuizzes: Quiz[] = [];
+  displayedColumns: string[] = ['title', 'group', 'schadule', 'participants'];
+  dataSource: any[] = [];
+
+  constructor(private _DashlistService: DashlistService) {}
+
   ngOnInit(): void {
-    this.gettops()
-    this.getquiz()
-  }
-  gettops(){
-    this._DashlistService.topstudents().subscribe({
-      next:(res)=>{
-  this.students=res
-  console.log(res)
-      }
-    })
-  }
-  getquiz(){
-    this._DashlistService.topquiz().subscribe({
-      next:(res)=>{
-  
-  console.log(res)
-      }
-    })
+    this.getCompletedQuiz();
+    this.getComingQuiz();
   }
 
+  getCompletedQuiz() {
+    this._DashlistService.topLastQuizzes().subscribe({
+      next: (res) => {
+        this.completedQuizzes = res;
+        this.dataSource = this.completedQuizzes;
+        console.log(this.completedQuizzes);
+      },
+    });
+  }
+  getComingQuiz() {
+    this._DashlistService.topquiz().subscribe({
+      next: (res) => {
+        this.IncomingQuizzes = res;
+      },
+    });
+  }
 }
--- src/app/feature/dashboard/services/dashlist.service.ts
+++ src/app/feature/dashboard/services/dashlist.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Quiz } from '../interfaces/quiz';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashlistService {
-
-  constructor(private _HttpClient:HttpClient) { }
-  topstudents():Observable<any>{
-  return this._HttpClient.get('student/top-five')
+  constructor(private _HttpClient: HttpClient) {}
+  topstudents(): Observable<any> {
+    return this._HttpClient.get('student/top-five');
   }
 
-topquiz():Observable<any>{
-  return this._HttpClient.get('quiz/incomming')
+  topquiz(): Observable<Quiz[]> {
+    return this._HttpClient.get<Quiz[]>('quiz/incomming');
+  }
+
+  topLastQuizzes(): Observable<Quiz[]> {
+    return this._HttpClient.get<Quiz[]>('quiz/completed');
   }
 }
