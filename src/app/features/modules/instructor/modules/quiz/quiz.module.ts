923a6d18c18fe79b13fa029cdce43c1bdae09311
reemWael123 <reemelkazaz@gmail.com>
2024-10-07 22:02:58 +0300

    first

--- src/app/features/modules/instructor/modules/quiz/quiz.module.ts
+++ src/app/features/modules/instructor/modules/quiz/quiz.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { QuizRoutingModule } from './quiz-routing.module';
 import { QuizComponent } from './quiz.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { QuizdetailsComponent } from './quizdetails/quizdetails.component';
 
 
 @NgModule({
   declarations: [
-    QuizComponent
+    QuizComponent,
+    QuizdetailsComponent
   ],
   imports: [
     CommonModule,
